66ca2dd27ac7af4b0788aa0c6ce9f9d1e16299e5
kundalili <105823291+kundalili@users.noreply.github.com>
2022-11-25 23:40:02 +0100

    button added

--- client/src/components/Login/LoginMidwife.js
+++ client/src/components/Login/LoginMidwife.js
@@ -55,7 +55,21 @@ export default function Login(props) {
                 <TextField placeholder='Type your email' value={data.email} onChange={e => setData({...data, email: e.target.value})} id="outlined-basic" label="Email" variant="outlined" />
             </div>
             <TextField placeholder='Type your password' value={data.password} onChange={e => setData({...data, password: e.target.value})} id="outlined-basic" label="Password" variant="outlined" />
-          <Button className='' variant="outlined" onClick={handleLogin}>Login</Button>
+            <button  type="submit" onClick={handleLogin}
+                        className='cursor-pointer 
+                        border-2 border-vividBlue 
+                        text-vividBlue 
+                        font-semibold 
+                        hover:border-2
+                        text-center w-[312px] 
+                        h-[68px] 
+                        outline-none 
+                        rounded-full 
+                        hover:text-white
+                        hover:bg-vividBlue 
+                        hover:border-vividBlue'>
+                        LOGIN
+                    </button>
         </div>  
       </>
         )
